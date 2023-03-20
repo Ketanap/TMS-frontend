25346f807fbc13cd5d322fa290c390a397260bf4
Mac004 <mac004@MacyMind-004.local>
2023-03-20 15:03:34 +0530

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
