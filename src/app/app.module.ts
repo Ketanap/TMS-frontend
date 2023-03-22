ed142a1c764cab263db86ef2624e03066747b6ee
Mac004 <mac004@MacyMind-004.local>
2023-03-22 11:22:48 +0530

    changes model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
-
+export class SignupComponent implements OnInit{
+@Input() id:any="";
+UserName="";
+Email="";
+Password="";
+Contact="";
+constructor(private http: HttpClient) {}
+ 
+ngOnInit(){ }
+ngOnChanges() {
+  console.log("signup" + this.id);
+  this.http.get('http://localhost:9090/user'+this.id).subscribe(data=>this.showData(data));
+}
+showData(data:any) {
+  this.UserName=data.UserName;
+  this.Email=data.Email;
+  this.Password=data.Password;
+  this.Contact=data.Contact;
+}
+OnSubmit()
+{
+  this.http.post('http://localhost:9090/user',
+  {id:this.id,UserName:this.UserName,Email:this.Email,Password:this.Password,Contact:this.Contact})
+  .subscribe(
+    data=>{location.reload();}
+  );
+}
 }
