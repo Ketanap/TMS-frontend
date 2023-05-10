74fdbc9949c84c010616a6d7f38f84e1f915397c
Mac004 <mac004@MacyMind-004.local>
2023-05-10 17:04:20 +0530

    Taskhistory

--- src/app/services/changestatus/changestatus.component.ts
+++ src/app/services/changestatus/changestatus.component.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-changestatus',
   templateUrl: './changestatus.component.html',
@@ -11,7 +10,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ChangestatusComponent implements OnInit {
   @Input()
   Taskid = "";
-  TaskDate = new Date().toLocaleDateString('en-GB'); editEvent: any;
+  TaskDate = new Date().toLocaleDateString('en-GB');
+  editEvent: any;
   UserId = "";
   ProjectId = "";
   StatusId = "";
@@ -26,48 +26,45 @@ export class ChangestatusComponent implements OnInit {
   data = {};
   User: any = [];
   Changedate = new Date().toLocaleDateString('en-GB');
-  
+  isStatusCompleted = false; // Added variable to track status completion
 
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {}
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {
-  }
- 
   ngOnInit(): void {
-    this.route.queryParams
-      .subscribe((params) => {
-        this.Taskid = params['id'];
-        var user = JSON.parse(localStorage.getItem("user") || "{}");
-        this.UserId = user.user.userid;
-        let api_key = user.token;
-        const headers = new HttpHeaders({
-          "Content-Type": "application/json",
-          'Authorization': `Bearer ${api_key}`
-        });
-
-        const requestOptions = { headers: headers };
-        this.http
-          .get("http://localhost:9090/task/" + this.Taskid, requestOptions)
-          .subscribe((data) => this.showData(data));
-        this.http
-          .get("http://localhost:9090/taskstatus/", requestOptions)
-          .subscribe((data) => this.showStatus(data));
-        this.http
-          .get("http://localhost:9090/project/", requestOptions)
-          .subscribe((data) => this.showProject(data));
-        this.http
-          .get("http://localhost:9090/user/", requestOptions)
-          .subscribe((data) => this.showUser(data));
+    this.route.queryParams.subscribe((params) => {
+      this.Taskid = params['id'];
+      var user = JSON.parse(localStorage.getItem("user") || "{}");
+      this.UserId = user.user.userid;
+      let api_key = user.token;
+      const headers = new HttpHeaders({
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${api_key}`
       });
+
+      const requestOptions = { headers: headers };
+      this.http.get("http://localhost:9090/task/" + this.Taskid, requestOptions)
+        .subscribe((data) => this.showData(data));
+      this.http.get("http://localhost:9090/taskstatus/", requestOptions)
+        .subscribe((data) => this.showStatus(data));
+      this.http.get("http://localhost:9090/project/", requestOptions)
+        .subscribe((data) => this.showProject(data));
+      this.http.get("http://localhost:9090/user/", requestOptions)
+        .subscribe((data) => this.showUser(data));
+    });
   }
+
   showStatus(data: any) {
     this.Status = data;
   }
+
   showProject(data: any) {
     this.Project = data;
   }
+
   showUser(data: any) {
     this.User = data;
   }
+
   showData(data: any) {
     if (data) {
       console.log(data);
@@ -81,15 +78,13 @@ export class ChangestatusComponent implements OnInit {
       this.ActualTime = data.actualtime;
       this.DueDate = data.duedate;
       this.CompletedDate = data.completeddate;
+      this.isStatusCompleted = this.StatusId === 'completed'; // Update the isStatusCompleted variable based on the initial status
     }
-
   }
 
   editClick(id: number) {
-
     this.editEvent.emit(id);
   }
-
 
   OnSubmit() {
     var user = JSON.parse(localStorage.getItem("user") || "{}");
@@ -106,10 +101,10 @@ export class ChangestatusComponent implements OnInit {
       // Log the user out or redirect them to the login page
       return;
     }
-    
 
     console.log(headers);
-    if (this.Taskid) {
+    if
+ (this.Taskid) {
       this.http.put('http://localhost:9090/task/' + this.Taskid,
         { taskdate: this.TaskDate, userid: this.UserId, projectid: this.ProjectId, statusid: this.StatusId, description: this.Description, expectedtime: this.ExpectedTime, actualtime: this.ActualTime, duedate: this.DueDate, completeddate: this.Changedate, roleid: 2 }, requestOptions)
         .subscribe(
